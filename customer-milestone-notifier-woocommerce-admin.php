ddf15a8ca4f10449bde4acb512b00f3fb4afc8fb
ronakganagtra9 <ronakganatra9@gmail.com>
2019-04-03 15:50:09 +0530

    formatting.

--- customer-milestone-notifier-woocommerce-admin.php
+++ customer-milestone-notifier-woocommerce-admin.php
@@ -22,6 +22,7 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
 /**
  * WooCommerce_Activity_Panel_Customer_Milestone_Notification
  */
@@ -29,7 +30,7 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 	/**
 	 * Name of the "first customer" note.
 	 */
-    const FIRST_CUSTOMER_NOTE_NAME = 'woocommerce-admin-first-customer';
+	const FIRST_CUSTOMER_NOTE_NAME = 'woocommerce-admin-first-customer';
 
 	/**
 	 * Name of the "ten customers" note.
@@ -77,8 +78,9 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 	 */
 	public function get_customers_count() {
 		if ( is_null( $this->customers_count ) ) {
-			$this->customers_count  = count( get_users( array( 'role' => 'Customer' ) ) );
+			$this->customers_count = count( get_users( array( 'role' => 'Customer' ) ) );
 		}
+
 		return $this->customers_count;
 	}
 
@@ -95,12 +97,12 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 			return;
 		}
 		$customers_count = $this->get_customers_count();
-		$data_store = WC_Data_Store::load( 'admin-note' );
+		$data_store      = WC_Data_Store::load( 'admin-note' );
 		if ( 1 === $customers_count ) {
 			// Add the first customer note.
 			// First, see if we've already created this kind of note so we don't do it again.
 			$note_ids = $data_store->get_notes_with_name( self::FIRST_CUSTOMER_NOTE_NAME );
-			foreach( (array) $note_ids as $note_id ) {
+			foreach ( (array) $note_ids as $note_id ) {
 				$note         = WC_Admin_Notes::get_note( $note_id );
 				$content_data = $note->get_content_data();
 				if ( property_exists( $content_data, 'first_customer' ) ) {
@@ -109,15 +111,15 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 			}
 
 			// Otherwise, add the note
-			$activated_time = current_time( 'timestamp', 0 );
+			$activated_time           = current_time( 'timestamp', 0 );
 			$activated_time_formatted = date( 'F jS', $activated_time );
-			$note = new WC_Admin_Note();
+			$note                     = new WC_Admin_Note();
 			$note->set_title( __( 'First Customer', 'customer-milestone-notifier-woocommerce-admin' ) );
 			$note->set_content(
 				__( 'Congratulations on getting your first customer on your store..!', 'customer-milestone-notifier-woocommerce-admin' )
 			);
 			$note->set_content_data( (object) array(
-				'first_customer'     => true,
+				'first_customer'      => true,
 				'activated'           => $activated_time,
 				'activated_formatted' => $activated_time_formatted,
 			) );
@@ -135,7 +137,7 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 			// Add the ten customers note.
 			// First, see if we've already created this kind of note so we don't do it again.
 			$note_ids = $data_store->get_notes_with_name( self::TEN_CUSTOMER_NOTE_NAME );
-			foreach( (array) $note_ids as $note_id ) {
+			foreach ( (array) $note_ids as $note_id ) {
 				$note         = WC_Admin_Notes::get_note( $note_id );
 				$content_data = $note->get_content_data();
 				if ( property_exists( $content_data, 'tenth_customer' ) ) {
@@ -144,9 +146,9 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 			}
 
 			// Otherwise, add the note
-			$activated_time = current_time( 'timestamp', 0 );
+			$activated_time           = current_time( 'timestamp', 0 );
 			$activated_time_formatted = date( 'F jS', $activated_time );
-			$note = new WC_Admin_Note();
+			$note                     = new WC_Admin_Note();
 			$note->set_title(
 				sprintf(
 					__( 'Congratulations on getting 10 customers on your store.!', 'customer-milestone-notifier-woocommerce-admin' )
@@ -156,7 +158,7 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 				__( "You've hit the 10 customers milestone! Look at you go. Browse some WooCommerce success stories for inspiration.", 'customer-milestone-notifier-woocommerce-admin' )
 			);
 			$note->set_content_data( (object) array(
-				'tenth_customer'     => true,
+				'tenth_customer'      => true,
 				'activated'           => $activated_time,
 				'activated_formatted' => $activated_time_formatted,
 			) );
@@ -170,65 +172,68 @@ class WooCommerce_Activity_Panel_Customer_Milestone_Notification {
 			$this->other_customer_milestones();
 		}
 	}
+
 	/**
 	 * Add milestone notes for other significant thresholds.
 	 */
 	public function other_customer_milestones() {
-		$customers_count      = $this->get_customers_count();
-		if(in_array($customers_count,$this->customer_milestones)){
-		// We only want one milestone note at any time.
-		WC_Admin_Notes::delete_notes_with_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
+		$customers_count = $this->get_customers_count();
+		if ( in_array( $customers_count, $this->customer_milestones ) ) {
+			// We only want one milestone note at any time.
+			WC_Admin_Notes::delete_notes_with_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
 
 
-		// Add the milestone note.
-		$activated_time = current_time( 'timestamp', 0 );
-		$activated_time_formatted = date( 'F jS', $activated_time );
-		$note = new WC_Admin_Note();
-		$note->set_title(
-			sprintf(
-			/* translators: Number of customer registered. */
-				__( 'Congratulations on processing %s customers..!', 'customer-milestone-notifier-woocommerce-admin' ),
-				wc_format_decimal( $customers_count )
-			)
-		);
-		$note->set_content(
-			__( 'Another order milestone! Take a look at your Customer Report to review your customer details.', 'customer-milestone-notifier-woocommerce-admin' )
-		);
-		$note->set_content_data( (object) array(
-			'other_customer'     => true,
-			'activated'           => $activated_time,
-			'activated_formatted' => $activated_time_formatted,
-		) );
-		$note->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
-		$note->set_icon( 'trophy' );
-		$note->set_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
-		$note->set_source( 'customer-milestone-notifier-woocommerce-admin' );
-		$note->add_action( 'review-customers', __( 'Review your custmers', 'customer-milestone-notifier-woocommerce-admin' ), '?page=wc-admin#/analytics/customers' );
-		$note->save();
+			// Add the milestone note.
+			$activated_time           = current_time( 'timestamp', 0 );
+			$activated_time_formatted = date( 'F jS', $activated_time );
+			$note                     = new WC_Admin_Note();
+			$note->set_title(
+				sprintf(
+				/* translators: Number of customer registered. */
+					__( 'Congratulations on processing %s customers..!', 'customer-milestone-notifier-woocommerce-admin' ),
+					wc_format_decimal( $customers_count )
+				)
+			);
+			$note->set_content(
+				__( 'Another order milestone! Take a look at your Customer Report to review your customer details.', 'customer-milestone-notifier-woocommerce-admin' )
+			);
+			$note->set_content_data( (object) array(
+				'other_customer'      => true,
+				'activated'           => $activated_time,
+				'activated_formatted' => $activated_time_formatted,
+			) );
+			$note->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+			$note->set_icon( 'trophy' );
+			$note->set_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
+			$note->set_source( 'customer-milestone-notifier-woocommerce-admin' );
+			$note->add_action( 'review-customers', __( 'Review your custmers', 'customer-milestone-notifier-woocommerce-admin' ), '?page=wc-admin#/analytics/customers' );
+			$note->save();
 		}
 	}
 
-    /**
-     * Removes any notes this plugin created.
-     */
-   public static function remove_customer_milestone_notifier_notes() {
-        if ( ! class_exists( 'WC_Admin_Notes' ) ) {
-            return;
-        }
+	/**
+	 * Removes any notes this plugin created.
+	 */
+	public static function remove_customer_milestone_notifier_notes() {
+		if ( ! class_exists( 'WC_Admin_Notes' ) ) {
+			return;
+		}
 
-        WC_Admin_Notes::delete_notes_with_name( self::FIRST_CUSTOMER_NOTE_NAME );
-        WC_Admin_Notes::delete_notes_with_name( self::TEN_CUSTOMER_NOTE_NAME );
-	    WC_Admin_Notes::delete_notes_with_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
-    }
+		WC_Admin_Notes::delete_notes_with_name( self::FIRST_CUSTOMER_NOTE_NAME );
+		WC_Admin_Notes::delete_notes_with_name( self::TEN_CUSTOMER_NOTE_NAME );
+		WC_Admin_Notes::delete_notes_with_name( self::CUSTOMERS_MILESTONE_NOTE_NAME );
+	}
 }
 
 function customer_milestone_notifier_activate() {
 }
+
 register_activation_hook( __FILE__, 'customer_milestone_notifier_activate' );
 
 function customer_milestone_notifier_deactivate() {
 	WooCommerce_Activity_Panel_Customer_Milestone_Notification::remove_customer_milestone_notifier_notes();
 }
+
 register_deactivation_hook( __FILE__, 'customer_milestone_notifier_deactivate' );
 
 new WooCommerce_Activity_Panel_Customer_Milestone_Notification();
